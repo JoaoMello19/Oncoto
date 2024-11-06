4eb9c0c9b5e081aa74bb197928cb75c6dd1e280a
JoaoMello <joaomgvitor@hotmail.com>
2024-11-06 15:25:20 -0300

    Finalizando o modal

--- scripts.js
+++ scripts.js
@@ -1,21 +1,17 @@
-// Obtenha o modal e o botão de abertura
+/** LÓGICA DE CONTROLE DO MODAL */
 const btnRules = document.getElementById("btn-rules");
 const rulesModal = document.getElementById("rules-modal");
 const closeModal = document.getElementById("close-modal");
 
-// Quando o usuário clicar no botão, o modal é exibido
-btnRules.onclick = function() {
-    rulesModal.style.display = "block";
-}
+btnRules.onclick = () => { rulesModal.style.display = "block"; }
 
-// Quando o usuário clicar no "X", o modal é fechado
-closeModal.onclick = function() {
-    rulesModal.style.display = "none";
-}
+closeModal.onclick = () => { rulesModal.style.display = "none"; }
 
-// Quando o usuário clicar fora do modal, ele também é fechado
-window.onclick = function(event) {
-    if (event.target == modal) {
+window.onclick = (event) => {
+    if (event.target == modal)
         rulesModal.style.display = "none";
-    }
 }
+
+
+/** LÓGICA DE CONTROLE DA TABELA */
+const tableCountries = document.getElementById("country-table");
